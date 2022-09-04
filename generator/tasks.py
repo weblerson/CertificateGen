cee119ed470166278231f79199e4bc5a429cb8eb
weblerson <zonejr4@gmail.com>
2022-09-04 15:49:27 -0400

    retry added

--- generator/tasks.py
+++ generator/tasks.py
@@ -5,11 +5,17 @@ from django.conf import settings
 
 from .utils import send_email, edit_image
 
-@shared_task
-def send_certificate(name: str, email: str):
+@shared_task(name='send_certificate', bind=True, max_retries=5, default_retry_delay=2)
+def send_certificate(self, name: str, email: str):
     image_name = sha256(f'{name}{email}'.encode()).hexdigest()
 
-    edit_image(name, image_name)
-    send_email(settings.TEMPLATE_PATH, 'Atestado', email, name=name, image_name=image_name)
+    try:
+        edit_image(name, image_name)
+        send_email(settings.TEMPLATE_PATH, 'Atestado', email, name=name, image_name=image_name)
 
-    return True
+        return True
+
+    except:
+        self.retry(countdown=2*self.request.retries)
+        
+        raise Exception('An error has occurred.')
